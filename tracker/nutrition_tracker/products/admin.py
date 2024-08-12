6b8162b1efd4aea70649dba0cb26370db2cbd7b3
nbelonogov <nbelonogov96@gmail.com>
2024-08-12 12:02:46 +0300

    Update admin.py

--- tracker/nutrition_tracker/products/admin.py
+++ tracker/nutrition_tracker/products/admin.py
@@ -4,4 +4,14 @@ from products.models import User, Product, ProductCategory
 
 admin.site.register(User)
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'proteins', 'fats', 'carbs', 'calories', 'category')
+    fields = ('id', 'name', 'proteins', 'fats', 'carbs', 'calories', 'category')
+    readonly_fields = ('id',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_filter = ('category',)
+
